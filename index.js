23acb96cab276629465469fddbf9ca85ebbbd205
EC2 Default User <ec2-user@ip-172-31-14-76.eu-west-2.compute.internal>
2017-04-28 08:39:01 +0000

    update index.js to handle multiple servers, move classifier script to trash

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ var greetings = ["hello", "hi", "ahoy", "hey", "greetings", "yo", "bonjour"];
  * on every message sent on a server where this bot is present
  */
 bot.on("message", msg => {
+	//check if there is an existing record for the server that this record originated from
+	//if not, function will create one
+	serverRecordExists(msg.guild.name);
 	//if the bot is @ mentioned, the message is a command and this conditional block is run
 	if (msg.isMentioned(bot.user)) {
 		console.log("message is: \n"+msg.content);
@@ -60,29 +63,29 @@ bot.on("message", msg => {
 			msg.react(bot.emojis.random())
 				.then(msg => console.log(`Reacted to ${msg.content}`))
 				.catch(console.error);
-		} else if (msg.content.includes("warn")) {
+		} else if (msg.content.includes("warn")) { //add content warning keyword for user
 			console.log("logging content warning request for "+msg.content.substring(27));
 			//var word = parse the message to find the word alone
-			addTrigger(msg.content.substring(27), msg.author.username);
-			fs.writeFile("triggers.json", JSON.stringify(triggers), (err) => {
+			addTrigger(msg.content.substring(27), msg.author.username, msg.guild.name);
+			fs.writeFile("triggers.json", JSON.stringify(triggers, null, "\t"), (err) => {
 				if (err) throw (err);
 				console.log("JSON updated!");
 			});
 			msg.reply("keyword added, I'll keep an eye out!")
 				.then(msg => console.log(`Sent a reply to ${msg.author.username}`))
 				.catch(console.error);
-		} else if (msg.content.includes("safe")) {
+		} else if (msg.content.includes("safe")) { //end content warning
 			console.log("the coast is clear for: "+msg.content.substring(27));
-			var trigger = checkTriggers(msg.content);
+			var trigger = checkTriggers(msg.content, msg.guild.name);
 			var usersToNotify = new Array();
-			for (var user in triggers) {
-				if (triggers[user].includes(trigger)) {
+			for (var user in triggers.servers[msg.guild.name]) {
+				if (triggers.servers[msg.guild.name][user].includes(trigger)) {
 					usersToNotify.push(user);
 				}
 			}
 			console.log("sounding the all-clear to the following users: "+usersToNotify);
 			for (var j = 1; j < usersToNotify.length; j++) {
-				bot.users.find("username", usersToNotify[j]).sendMessage("they've sounded the all-clear for "+trigger+" in channel "+msg.channel.name)
+				bot[msg.guild.name].users.find("username", usersToNotify[j]).sendMessage("they've sounded the all-clear for "+trigger+" in channel "+msg.channel.name)
 					.then(console.log("sending all-clear"))
 					.catch(console.error);
 			}
@@ -94,20 +97,20 @@ bot.on("message", msg => {
 		}
 	} //if mentioned
 	//if the bot isn't mentioned, it just reads the message, checking for keywords that have been registered with it
-	else if (checkTriggers(msg.content) != "none" && msg.author.username != "rob-bot" && msg.channel.type != "dm") {
+	else if (checkTriggers(msg.content, msg.guild.name) != "none" && msg.author.username != "rob-bot" && msg.channel.type != "dm") {
 		console.log("preparing content warning");
-		var trigger = checkTriggers(msg.content);
+		var trigger = checkTriggers(msg.content, msg.guild.name);
 		var usersToWarn = new Array();
-		for (var user in triggers) {
-			console.log("name = "+user+" value = "+triggers[user]+"\ntype test: "+typeof user);
-			if (triggers[user].includes(trigger)) {
+		for (var user in triggers.servers[msg.guild.name]) {
+			if (triggers.servers[msg.guild.name][user].includes(trigger)) {
 				usersToWarn.push(user);
 			}
 		}
 		console.log("warning the following users: "+usersToWarn);
 		for (var j = 1; j < usersToWarn.length; j++) {
 			console.log(usersToWarn[j]);
-			bot.users.find("username", usersToWarn[j]).sendMessage("content warning for "+trigger+" in channel "+msg.channel.name)
+
+			bot.guilds.find("name", msg.guild.name).users.find("username", usersToWarn[j]).sendMessage("content warning for "+trigger+" in channel "+msg.channel.name)
 				.then(console.log("sending content warning"))
 				.catch(console.error);
 			
@@ -117,27 +120,42 @@ bot.on("message", msg => {
 });
 
 /**
+ * Checks if a record of the server exists in the JSON, adds if not
+ */
+function serverRecordExists(serverName) {
+	if (!triggers.servers.hasOwnProperty(serverName)) {
+		triggers.servers[serverName] = {};
+		triggers.servers[serverName].all = new Array();
+		console.log("new server record created for "+serverName);
+		fs.writeFile("triggers.json", JSON.stringify(triggers), (err) => {
+			if (err) throw (err);
+			console.log("JSON updated!");
+		});
+	}	
+}
+
+/**
  * Adds a new trigger to the list of all triggers, as well as one for
  * the individual user who requested the warning
  */
-function addTrigger(trigger, username) {
-	if (!triggers.all.includes(trigger)) {
-		triggers.all.push(trigger);
+function addTrigger(trigger, username, server) {
+	if (!triggers.servers[server].all.includes(trigger)) {
+		triggers.servers[server].all.push(trigger);
 	}
-	if (triggers.hasOwnProperty(username)) {
+	if (triggers.servers[server].hasOwnProperty(username)) {
 		//add trigger to array where key == username
-		triggers[username].push(trigger);
+		triggers.servers[server][username].push(trigger);
 	} else {
-		triggers[username] = [trigger];
+		triggers.servers[server][username] = [trigger];
 	}
 }
 
 /**
  * Checks if a given message contains any of the trigger keywords
  */
-function checkTriggers(message) {
-	for (var i = 0; i < triggers.all.length; i++) {
-		var trigger = triggers.all[i];
+function checkTriggers(message, server) {
+	for (var i = 0; i < triggers.servers[server].all.length; i++) {
+		var trigger = triggers.servers[server].all[i];
 		if (message.includes(trigger)) {
 			return trigger;
 			//return the trigger and break off
